742c75cf4e4b985d0e2dc18d591094fa5c87f8df
AndyDeany <oneandydean@hotmail.com>
2017-01-16 16:10:45 +0000

    Add functions to change font family and size. Add functionality to change backdrop color. Refactor.

--- restyle.js
+++ restyle.js
@@ -1,9 +1,22 @@
-function recolor(text, background) {
-    var html = document.body.innerHTML
-    function replace(regex, replacement_string) {
-        html = html.replace(regex, replacement_string);
+function replace(regex, replacement_string, body=true) {
+    if (body) {
+        document.body.innerHTML = document.body.innerHTML.replace(regex, replacement_string);
+    } else {
+        document.head.innerHTML = document.head.innerHTML.replace(regex, replacement_string);
     }
+}
+
+function color(text, background, backdrop) {
     replace(/#000000/g, text);
     replace(/#EEEEEE/g, background);
-    document.body.innerHTML = html
+    replace(/#535353/, backdrop, false);
+}
+
+function font_type(link, text) {
+    replace(/font-family: Verdana/, "font-family: " + link);
+    replace(/font-family: courier/g, "font-family: " + text);
+}
+
+function font_size(pixels) {
+    replace(/font-size: 14px;/, "font-size: " + pixels);
 }
